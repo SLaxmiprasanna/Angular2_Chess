550847d385c15e06f3c0d88865df042f9b9e6300
SLaxmiprasanna <prasannasandumalla@gmail.com>
2017-06-09 11:07:16 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {CapturedPiecesComponent} from "../captured-pieces/capturedpiecescomponent";
+import {ChessAppComponent} from "../chess-app/chess.component";
+import {ChessBoardComponent} from "../chessboard/chessboardcomponent";
+import {ChessEngineAPI} from "../engine/chessboardUI"
+import ChessboardUI = ChessEngineAPI.ChessboardUI;
+import {FieldComponent} from "../field/fieldcomponent";
+import {HistoryComponent} from "../history/historycomponent";
+
+import {RowComponent} from "../row/rowcomponent";
 
 @NgModule({
   declarations: [
-    AppComponent
+    ChessAppComponent,
+    
+    ChessBoardComponent,
+    CapturedPiecesComponent,
+    RowComponent,
+    FieldComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: CapturedPiecesComponent,   useClass:    CapturedPiecesComponent },
+    { provide: ChessAppComponent,   useClass:    ChessAppComponent },
+    { provide: ChessBoardComponent,   useClass:    ChessBoardComponent },
+    { provide: ChessboardUI,   useClass:    ChessboardUI },
+    { provide: FieldComponent,   useClass:    FieldComponent },
+    { provide: RowComponent,   useClass:    RowComponent },
+    { provide: HistoryComponent,   useClass:    HistoryComponent },
+    
+  ],
+  bootstrap: [ChessAppComponent]
 })
 export class AppModule { }
